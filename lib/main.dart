6d2d1691dfecbc27f835ee68524f3f82dcd1ff90
Agustin Sampietro <sampietroagustin@gmail.com>
2021-03-08 19:31:16 -0300

    test8

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        title: 'Material',
+        title: 'Material app',
         debugShowCheckedModeBanner: false,
         theme: miTema,
         home: TabsPage(),
